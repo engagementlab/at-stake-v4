99185ff3e6e586b1afe3710fe3cc50f598ef74a4
Ralph Drake <ralph_drake@emerson.edu>
2019-10-31 11:32:11 -0400

    Style fix in Decks.jsx

--- client/src/components/Lobby/Decks.jsx
+++ client/src/components/Lobby/Decks.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 
 class Decks extends Component {
-
   callback(deck) { }
 
 
@@ -11,21 +10,21 @@ class Decks extends Component {
       // {i === 0 ?( <div>Room Code: </div>) : (null)}
       this.props.decks && this.props.decks.map((deck, i) => {
         return (
-          <a href="#" onClick={() => this.props.callback(deck)}>
+          <button type="button" onClick={() => this.props.callback(deck)}>
             <h2 className="deck">
               <div className="header">
                 <span>{deck.name}</span>
                 {/*
-                  {{{cloudinaryUrl 'v1540488090/at-stake/icons/magnify'
-                      className='open' format='svg' alt='Magnifying glass icon'}}}
-                */}
+                {{{cloudinaryUrl 'v1540488090/at-stake/icons/magnify'
+                    className='open' format='svg' alt='Magnifying glass icon'}}}
+              */}
               </div>
 
               <div className="description">
                 {/* {deck.description} */}
               </div>
             </h2>
-          </a>
+          </button>
         );
       })
     );
