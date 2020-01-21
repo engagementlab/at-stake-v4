c97dbd830071caa003e33b328289cfdb92ae979d
Johnny Richardson <johnny.c.rich@gmail.com>
2020-01-21 13:11:25 -0500

    Separate client/server dirs, remove deprecated public dir in server/, update prettier/eslint config and VScode config to prettify on save, added sound and image assets to client.

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -2,20 +2,16 @@ module.exports = {
   env: {
     commonjs: true,
     es6: true,
-    node: true,
+    node: true
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ['airbnb-base'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
-    _: 'readonly',
+    _: 'readonly'
   },
   parserOptions: {
-    ecmaVersion: 2018,
+    ecmaVersion: 2018
   },
-  "ignorePatterns": ["client/.eslintrc.js"],
-  rules: {
-  },
+  ignorePatterns: ['client/.eslintrc.js']
 };
