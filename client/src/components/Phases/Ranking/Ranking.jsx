a0f285a31a6699a18a8a4eeb0d2a4b5e52f863cb
Johnny Richardson <johnny.c.rich@gmail.com>
2020-01-21 15:02:00 -0500

    Started on end game screen, syntax fixes for server/app.js, added node-sass for client.

--- client/src/components/Phases/Ranking/Ranking.jsx
+++ client/src/components/Phases/Ranking/Ranking.jsx
@@ -4,76 +4,80 @@ import SocketContext from '../../../SocketContext';
 import Instructions from '../../Shared/Instructions/Instructions';
 import GameData from '../../../GameData';
 
-class Ranking extends PureComponent { 
+class Ranking extends PureComponent {
   constructor(props) {
     super(props);
 
     this.state = {
-      screenIndex: 1,
+      gameWon: false,
+
       ratingEquity: 1,
       ratingInclusivity: 1,
       ratingCreativity: 1,
+
+      screenIndex: 1,
+
+      showResult: false
     };
 
     this.needsMet = 0;
-}
+  }
 
   componentDidMount = () => {
-    
     this.socket = this.props.socket;
 
     /* Socket Listeners */
 
     // Tell facilitator all players ready for intros
-    this.socket.on('game:ready', () => {
+    // this.socket.on('game:ready', () => {
+    //   this.setState({ allPlayersReady: true });
+    // });
 
-      this.setState({ allPlayersReady: true });
-
+    // Show game result
+    this.socket.on('game:end', data => {
+      this.setState({ showResult: true, gameWon: data.won });
     });
-
-  }
+  };
 
   componentWillUnmount = () => {
     console.log('Ranking will unmount');
-  }
+  };
 
   nextScreen() {
-
+    debugger;
     this.setState({
       screenIndex: this.state.screenIndex + 1
     });
-
   }
 
   playerMetGoal(id) {
-
-    this.socket.emit('player:met_goal',
-    GameData.get().assemble({
+    this.socket.emit(
+      'player:met_goal',
+      GameData.get().assemble({
         uid: id
-    }));
-
+      })
+    );
   }
 
   playerMetNeed(id, index) {
-
     // Cache increase for ranking submission
     this.needsMet++;
 
-    this.socket.emit('player:met_need',
-    GameData.get().assemble({
+    this.socket.emit(
+      'player:met_need',
+      GameData.get().assemble({
         uid: id,
         index: index
-    }));
-
+      })
+    );
   }
 
   // Ranking sliders update
   sliderChange(evt, index) {
-
     let val = evt.currentTarget.value;
     let state = {};
 
-    switch(index) {
+    switch (index) {
       default:
       case 0:
         state = { ratingEquity: val };
@@ -87,162 +91,273 @@ class Ranking extends PureComponent {
     }
 
     this.setState(state);
-
   }
 
   submitRating() {
+    let ratingSum =
+      this.state.ratingCreativity +
+      this.state.ratingInclusivity +
+      this.state.ratingEquity;
 
+    this.socket.emit(
+      'game:ranking',
+      GameData.get().assemble({ needs: this.needsMet, rating: ratingSum })
+    );
   }
 
-  render () {
+  endGame() {
+    this.socket.emit('game:end', GameData.get().assemble());
+  }
 
-    const { isFacilitator, ratingEquity, ratingCreativity, ratingInclusivity, screenIndex } = this.state,
-          data = this.props.data;
-    
+  render() {
+    const {
+        gameWon,
+        isFacilitator,
+        ratingEquity,
+        ratingCreativity,
+        ratingInclusivity,
+        screenIndex,
+        showResult
+      } = this.state,
+      data = this.props.data;
+
     // RANKING UI
     return (
       <div id="ranking">
-      
         {/* WAIT FOR RANK (Non-fac) */}
-        {!isFacilitator && 
+        {!(isFacilitator && showResult) && (
           <div className="screen player">
-        
-            <Instructions
-              heading="Call To Vote"
-              />
-        
+            <Instructions heading="Call To Vote" />
+
             <div id="content" className="content">
-        
               <h1>Everyone agreed with the proposal!</h1>
               Wait as the facilitator reviews.
+            </div>
 
-            </div>
-        
-            <div id="results">
-                
-            </div>
-            
+            <div id="results"></div>
           </div>
-        }
-      
+        )}
+
         {/* RANKING */}
-        <div className="screen decider">
-      
-          <Instructions
+        {!showResult && (
+          <div className="screen decider">
+            <Instructions
               show={isFacilitator}
               heading="Vote Ranking"
               body="Rate how well the team's proposal meets the criteria below."
-          />
-      
-          {/* Goals */}
-          {screenIndex === 0 &&
-            <div id="pt1">
-              <h2>Did the team meet any secret goals?</h2>
+            />
 
-              {/* Show all player secret goals */}
-              {data.players.map((player, i) => {
+            {/* Goals */}
+            {screenIndex === 0 && (
+              <div id="pt1">
+                <h2>Did the team meet any secret goals?</h2>
 
-                return (
-                  <div key={player.uid} className="check toggle">
-                    <p>{player.username}</p>
-                    <div className="goal">{player.secretGoal}</div>
-
-                    <label className="switch">
-                        {i === 0 &&
-                          <span className="tooltip-content">When a player meets their secret goal, you can check it off. They'll score extra points at the end of the game.</span>
-                        }
-
-                        {player.goalMet ?
-                          <input type="checkbox" checked="checked" disabled="disabled" />
-                          :
-                          <input type="checkbox" onClick={ () => this.playerMetGoal(player.uid) } />
-                        }
-                        <span className="slider round"></span>
-                    </label>
-                  </div>
-                )
-
-              })}
-            </div>
-          }
-
-          {/* Needs */}
-          {screenIndex === 1 &&
-            <div id="pt2" className="form">
-              <h2>Did the team meet their needs?</h2>
-
-              {data.players && data.players.map((player, i) => {
-                
-                return (
-                  <div key={player.uid} className="toggle">
+                {/* Show all player secret goals */}
+                {data.players.map((player, i) => {
+                  return (
+                    <div key={player.uid} className="check toggle">
                       <p>{player.username}</p>
-                      <div className="needs"> 
+                      <div className="goal">{player.secretGoal}</div>
+
+                      <label className="switch">
+                        {i === 0 && (
+                          <span className="tooltip-content">
+                            When a player meets their secret goal, you can check
+                            it off. They'll score extra points at the end of the
+                            game.
+                          </span>
+                        )}
+
+                        {player.goalMet ? (
+                          <input
+                            type="checkbox"
+                            checked="checked"
+                            disabled="disabled"
+                          />
+                        ) : (
+                          <input
+                            type="checkbox"
+                            onClick={() => this.playerMetGoal(player.uid)}
+                          />
+                        )}
+                        <span className="slider round"></span>
+                      </label>
+                    </div>
+                  );
+                })}
+              </div>
+            )}
+
+            {/* Needs */}
+            {screenIndex === 1 && (
+              <div id="pt2" className="form">
+                <h2>Did the team meet their needs?</h2>
+
+                {data.players &&
+                  data.players.map((player, i) => {
+                    return (
+                      <div key={player.uid} className="toggle">
+                        <p>{player.username}</p>
+                        <div className="needs">
                           <div className="need">
+                            <label className="switch">
+                              <input
+                                type="checkbox"
+                                onClick={() =>
+                                  this.playerMetNeed(player.uid, 0)
+                                }
+                              />
+                              <span className="slider round"></span>
+                            </label>
 
-                              <label className="switch">
-                                  <input type="checkbox" onClick={ () => this.playerMetNeed(player.uid, 0) } />
-                                  <span className="slider round"></span>
-                              </label>
-                              
-                              <span>
-                                {player.needs ? player.needs[0] : 'Example need 1'}
-                              </span>
-
+                            <span>
+                              {player.needs
+                                ? player.needs[0]
+                                : 'Example need 1'}
+                            </span>
                           </div>
                           <div className="need">
+                            <label className="switch">
+                              <input
+                                type="checkbox"
+                                onClick={() =>
+                                  this.playerMetNeed(player.uid, 1)
+                                }
+                              />
+                              <span className="slider round"></span>
+                            </label>
 
-                              <label className="switch">
-                                <input type="checkbox" onClick={ () => this.playerMetNeed(player.uid, 1) } />
-                                  <span className="slider round"></span>
-                              </label>
-                              
-                              <span>
-                                {player.needs ? player.needs[1] : 'Example need 2'}
-                              </span>
-
+                            <span>
+                              {player.needs
+                                ? player.needs[1]
+                                : 'Example need 2'}
+                            </span>
                           </div>
-                      </div>      
-                      <hr />
-                  </div>
-          )
-              })}
-
-        </div>
-          }
-
-          {/* Rank */}
-          {screenIndex === 2 &&
-            <div id="pt3">
-
-              <h2>Equity</h2>
-              <input type="range" id="equity" min="1" max="5" step="1" value={ratingEquity} onChange={(e) => this.sliderChange(e, 0) } />
-              <div className="labels">
-                  <span>1</span><span>2</span><span>3</span><span>4</span><span>5</span>
+                        </div>
+                        <hr />
+                      </div>
+                    );
+                  })}
               </div>
+            )}
 
-              <h2>Inclusivity</h2>
-              <input type="range" id="inclusivity" min="1" max="5" step="1" value={ratingInclusivity} onChange={(e) => this.sliderChange(e, 1)  } />
-              <div className="labels">
-                  <span>1</span><span>2</span><span>3</span><span>4</span><span>5</span>
+            {/* Rank */}
+            {screenIndex === 2 && (
+              <div id="pt3">
+                <h2>Equity</h2>
+                <input
+                  type="range"
+                  id="equity"
+                  min="1"
+                  max="5"
+                  step="1"
+                  value={ratingEquity}
+                  onChange={e => this.sliderChange(e, 0)}
+                />
+                <div className="labels">
+                  <span>1</span>
+                  <span>2</span>
+                  <span>3</span>
+                  <span>4</span>
+                  <span>5</span>
+                </div>
+
+                <h2>Inclusivity</h2>
+                <input
+                  type="range"
+                  id="inclusivity"
+                  min="1"
+                  max="5"
+                  step="1"
+                  value={ratingInclusivity}
+                  onChange={e => this.sliderChange(e, 1)}
+                />
+                <div className="labels">
+                  <span>1</span>
+                  <span>2</span>
+                  <span>3</span>
+                  <span>4</span>
+                  <span>5</span>
+                </div>
+
+                <h2>Creativity</h2>
+                <input
+                  type="range"
+                  id="creativity"
+                  min="1"
+                  max="5"
+                  step="1"
+                  value={ratingCreativity}
+                  onChange={e => this.sliderChange(e, 2)}
+                />
+                <div className="labels">
+                  <span>1</span>
+                  <span>2</span>
+                  <span>3</span>
+                  <span>4</span>
+                  <span>5</span>
+                </div>
               </div>
+            )}
 
-              <h2>Creativity</h2>
-              <input type="range" id="creativity" min="1" max="5" step="1" value={ratingCreativity} onChange={(e) => this.sliderChange(e, 2) } />
-              <div className="labels">
-                  <span>1</span><span>2</span><span>3</span><span>4</span><span>5</span>
-              </div>
-
+            <button
+              type="submit"
+              name="submit"
+              onClick={() => {
+                screenIndex === 2 ? this.submitRating() : this.nextScreen();
+              }}
+            >
+              {screenIndex === 2 ? 'Submit' : 'Continue'}
+            </button>
           </div>
-          }
-      
-          <button type="submit" name="submit" onClick={ () => { 
-            screenIndex === 2 ? this.submitRating () : this.nextScreen()
-          }}>
-            { screenIndex === 2 ? 'Submit' : 'Continue' }
-          </button>
-      
-        </div>
-      
+        )}
+
+        {/* END */}
+        {showResult && (
+          <div id="end">
+            {/* Start over btn */}
+            {isFacilitator && (
+              <button
+                id="btn-start-over"
+                className="btn submit decider"
+                type="submit"
+                onClick={() => {
+                  this.endGame();
+                }}
+              >
+                Play Again
+              </button>
+            )}
+
+            {/* Game won/not won */}
+            {gameWon ? (
+              <div>
+                <img
+                  src="https://res.cloudinary.com/engagement-lab-home/image/upload/c_scale,f_auto,w_425/v1541442374/at-stake/bg/win"
+                  alt="Win image"
+                />
+                <h1>Congratulations</h1>
+                <div className="text">
+                  Together you've managed to bring a little peace and stability
+                  back to the world. Maybe there is hope for the future.
+                </div>
+              </div>
+            ) : (
+              <div>
+                <img
+                  src="https://res.cloudinary.com/engagement-lab-home/image/upload/c_scale,f_auto,w_425/v1541442374/at-stake/bg/lose"
+                  alt="Lose image"
+                />
+                <h1>Tragedy</h1>
+                <div className="text">
+                  Your team wasn't able to present a solution that fixed the
+                  problem. Reflect with your group and the facilitator about
+                  what went wrong.
+                </div>
+              </div>
+            )}
+          </div>
+        )}
       </div>
     );
   }
@@ -252,6 +367,6 @@ const RankingWithSocket = props => (
   <SocketContext.Consumer>
     {socket => <Ranking {...props} socket={socket} />}
   </SocketContext.Consumer>
-)
+);
 
 export default RankingWithSocket;
--- server/app.js
+++ server/app.js
@@ -11,70 +11,65 @@
  */
 
 // Load .env vars
-import {
-  format as _format,
-  createLogger,
-  transports as _transports,
-} from 'winston';
-import { json, urlencoded } from 'body-parser';
-import { connect, connection } from 'mongoose';
-
-import { start } from '@engagementlab/el-bootstrapper';
-import express from 'express';
-
 if (process.env.NODE_ENV !== 'test') require('dotenv').config();
 
-const logFormat = _format.combine(
-  _format.colorize(),
-  _format.timestamp(),
-  _format.align(),
-  _format.printf((info) => {
+const winston = require('winston'),
+  bodyParser = require('body-parser'),
+  mongoose = require('mongoose'),
+  bootstrap = require('@engagementlab/el-bootstrapper'),
+  express = require('express');
+
+const logFormat = winston.format.combine(
+  winston.format.colorize(),
+  winston.format.timestamp(),
+  winston.format.align(),
+  winston.format.printf((info) => {
     const {
-      timestamp, level, message, ...args
+      timestamp,
+      level,
+      message,
+      ...args
     } = info;
 
     const ts = timestamp.slice(0, 19).replace('T', ' ');
-    return `${ts} [${level}]: ${message} ${
-      Object.keys(args).length ? JSON.stringify(args, null, 2) : ''
-    }`;
+    return `${ts} [${level}]: ${message} ${Object.keys(args).length ? JSON.stringify(args, null, 2) : ''}`;
   }),
 );
 
 // Globals
 _ = require('underscore');
 
-logger = createLogger({
+logger = winston.createLogger({
   level: 'info',
   format: logFormat,
-  transports: [new _transports.Console()],
+  transports: [new winston.transports.Console()],
 });
 
 const app = express();
 
 // for parsing application/json
-app.use(json());
+app.use(bodyParser.json());
 
 // for parsing application/xwww-
 app.use(
-  urlencoded({
+  bodyParser.urlencoded({
     extended: true,
   }),
 );
 
-start(
+bootstrap.start(
   './config.json',
   app,
-  `${__dirname}/`,
-  {
+  `${__dirname}/`, {
     name: '@Stake CMS',
   },
   () => {
-    connect('mongodb://localhost/at-stake', {
+    mongoose.connect('mongodb://localhost/at-stake', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
 
-    const db = connection;
+    const db = mongoose.connection;
     db.on('error', console.error.bind(console, 'connection error:'));
 
     // Load sockets and serve http
