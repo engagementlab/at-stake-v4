03d877999187928932ec9b06ab59c82cae1ae5e9
Ralph Drake <ralph_drake@emerson.edu>
2020-01-31 16:41:06 -0500

    Updated Meet, Instructions, and Speech components to render using Bootstrap

--- client/src/components/Shared/Instructions/Instructions.jsx
+++ client/src/components/Shared/Instructions/Instructions.jsx
@@ -1,32 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-// import { Test } from './Instructions.styles';
+
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const Instructions = (props) => {
-  const { show, heading, body } = props;
-
-  if (!show) return null;
+  const { heading, body } = props;
 
   return (
-    <div className="InstructionsWrapper">
-      <h3>{heading}</h3>
-      <p>{body}</p>
-    </div>
+    <>
+      <Row>
+        <Col>
+          <h3>{heading}</h3>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <p>{body}</p>
+        </Col>
+      </Row>
+    </>
   );
 };
 
 Instructions.propTypes = {
   body: PropTypes.string,
   heading: PropTypes.string,
-  show: PropTypes.bool,
   subBody: PropTypes.string,
 };
 
 Instructions.defaultProps = {
-  body: 'Body Text',
-  heading: 'Instructions',
-  show: false,
-  subBody: 'Sub-body Text',
+  body: 'Default Body Text',
+  heading: 'Default Instructions',
+  subBody: 'Default Sub-body Text',
 };
 
 export default Instructions;
